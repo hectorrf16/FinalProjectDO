73f2407eec552577dceb13b8e04c054f49befb0d
hectorrf16 <hectoor.16@gmail.com>
2023-05-31 21:38:57 +0200

    muting all docker runs

--- docker/runs/grafanarun.sh
+++ docker/runs/grafanarun.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# set -e
-adduser --disabled-password --gecos "" hectorrf16
+adduser --disabled-password --gecos "" hectorrf16 > /dev/null 2>&1
 
-cp /home/tmp/server.crt /tmp/
-cp /home/tmp/server.csr /tmp/
+cp /home/tmp/server.crt /tmp/ > /dev/null 2>&1
+cp /home/tmp/server.csr /tmp/ > /dev/null 2>&1
 # chown grafana /tmp/server.*
-echo "Finished"
--- docker/runs/pgrun.sh
+++ docker/runs/pgrun.sh
@@ -1,9 +1,8 @@
 # !/bin/bash
 
-sleep 5
-cd /var/lib/pgadmin/storage
-mkdir ./hectoor.16_icloud.com
-chmod 700 ./hectoor.16_icloud.com
-cp /tmp/pgpass ./hectoor.16_icloud.com/pgpass
-chmod 600 ./hectoor.16_icloud.com/pgpass
-echo "Finished"
+sleep 5 > /dev/null 2>&1
+cd /var/lib/pgadmin/storage > /dev/null 2>&1
+mkdir ./hectoor.16_icloud.com > /dev/null 2>&1
+chmod 700 ./hectoor.16_icloud.com > /dev/null 2>&1
+cp /tmp/pgpass ./hectoor.16_icloud.com/pgpass > /dev/null 2>&1
+chmod 600 ./hectoor.16_icloud.com/pgpass > /dev/null 2>&1
